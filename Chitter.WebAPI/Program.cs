e1865e22e41444d85c8d21c86cae0d05a722f558
CassFaith13 <cassandra.faith@icloud.com>
2022-12-26 20:38:31 -0500

    dbcontext

--- Chitter.WebAPI/Program.cs
+++ Chitter.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Chitter.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Connection String
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
