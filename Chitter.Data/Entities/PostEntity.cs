acee16c502efb04bb34c806f20c213651ab26266
CassFaith13 <cassandra.faith@icloud.com>
2022-12-27 00:09:53 -0500

    chweets entity/controller

---
+++ Chitter.Data/Entities/PostEntity.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Chitter.Data.Entities
+{
+    public class PostEntity
+    {
+        [Key]
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        [Required]
+        public string? Text { get; set; }
+        // virtual list comments
+        public List<CommentEntity> Comments { get; set; }
+        // virtual list likes
+        public List<LikeEntity> Likes { get; set; }
+        public Guid AuthorID { get; set; }
+    }
+}
---
+++ Chitter.WebAPI/Controllers/LikeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chitter.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LikeController : ControllerBase
+    {
+        
+    }
+}
